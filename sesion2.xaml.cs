6982658d7cbfecb710dce3664e5d4013143b0b50
JoseM135 <93793078+MendozaSepulvedaCode@users.noreply.github.com>
2022-05-10 14:51:53 -0500

    Add files via upload

---
+++ sesion2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myapp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class sesion2 : ContentPage
+    {
+        public sesion2(String n, String a, String c)
+        {
+            InitializeComponent();
+        }
+    }
+}
